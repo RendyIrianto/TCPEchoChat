0d15ac868d90bb4336f12404a9178cd58f7623f8
RendyIrianto <90725398+RendyIrianto@users.noreply.github.com>
2025-07-06 14:04:13 -0400

    Adding exceptions

--- client.py
+++ client.py
@@ -9,9 +9,14 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((HOST, PORT))
 
+name = input("Enter your name:")
+
+client.sendall(name.encode())
+
 while True:
     message = input("You: ")
     if message.lower() == "exit":
+        print("Goodbye")
         break
     client.sendall(message.encode())
     data = client.recv(1024)
--- server.py
+++ server.py
@@ -5,15 +5,27 @@ HOST = "127.0.0.1"
 
 PORT = 65432
 
+clients = []
+
 def handle_client(conn, addr):
-    print(f"New connection from {addr}")
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        conn.sendall(data)
-    conn.close()
-    print(f"Connection with {addr} closed")
+    try:
+        print(f"New connection from {addr}")
+        while True:
+            name = conn.recv(1024).decode
+            print(name)
+            if not name:
+                break
+            conn.sendall(name)
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+    finally:
+        clients.remove(conn)
+        conn.close()
+        print(f"Connection with {addr} closed")
+
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -21,7 +33,14 @@ server.listen()
 
 print("Server is listening...")
 
-while True:
-    conn, addr = server.accept()
-    thread = threading.Thread(target=handle_client, args=(conn, addr))
-    thread.start()
+try:
+    while True:
+        conn, addr = server.accept()
+        clients.append(conn)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(clients)
+        
+except KeyboardInterrupt:
+    print("\nShutting down server...")
+    server.close()
